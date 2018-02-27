093673d5bb5e5db573f2f33c53d7a309c80aecc2
이정훈 <sksljh2091@naver.com>
2018-02-27 19:35:02 +0900

    relesase

--- EUDEditorUpdate/EUDEditorUpdate/Form1.Designer.vb
+++ EUDEditorUpdate/EUDEditorUpdate/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,11 +20,63 @@ Partial Class Form1
     '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
     '수정하려면 Windows Form 디자이너를 사용하십시오.  
     '코드 편집기에서는 수정하지 마세요.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 52)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(339, 23)
+        Me.ProgressBar1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(104, 20)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "다운로드 진행"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(86, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "LastVersion"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
-    End Sub
+        Me.ClientSize = New System.Drawing.Size(367, 91)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "EUDEditorUpdate"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
 
+    End Sub
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
 End Class
--- EUDEditorUpdate/EUDEditorUpdate/Form1.vb
+++ EUDEditorUpdate/EUDEditorUpdate/Form1.vb
@@ -1,3 +1,127 @@
-﻿Public Class Form1
+﻿Imports System.Net
+Imports System.IO
+Imports System.IO.Compression
 
+Public Class Form1
+    Dim status As Integer = 0
+    Dim lastver As String
+    Dim lastverurl As String
+    Dim currrentver As String
+
+    Dim folder As String = My.Application.Info.DirectoryPath
+    Private Sub Download(filename As String, savedfilename As String)
+        Dim Client As New WebClient
+        Dim StrDownUrl As String = filename
+        Dim StrDownFolder As String = folder & savedfilename
+
+
+        ServicePointManager.Expect100Continue = True
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12
+
+        '파일다운로드
+        '다른 다운로드 명령이 있으나 진행율을 표시하려면 DownloadFileAsync 을 사용해야 함
+        Client.DownloadFileAsync(New Uri(StrDownUrl), StrDownFolder)
+
+        'down1 이라는 이름으로 이벤트 생성
+        AddHandler Client.DownloadProgressChanged, AddressOf down1
+
+        'downok 이라는 이름으로 이벤트 생성
+        AddHandler Client.DownloadFileCompleted, AddressOf downok
+    End Sub
+
+
+    Private Sub down1(sender As System.Object, e As DownloadProgressChangedEventArgs)
+        Dim StrTxt As String
+        StrTxt = e.UserState & " DownLoad " & e.BytesReceived & "byte/" & e.TotalBytesToReceive & "byte"
+        LabelTxt(StrTxt)
+
+        With ProgressBar1
+            .Maximum = e.TotalBytesToReceive  '프로그래스바에 최대범위를 파일용량으로 넣음
+            .Value = e.BytesReceived          '가져오는 바이트양을 넣음
+        End With
+
+    End Sub
+
+
+    Sub DeleteFilesFromFolder(Folder As String)
+        If Directory.Exists(Folder) Then
+            For Each _file As String In Directory.GetFiles(Folder)
+                If _file <> Folder & "\EUDEditorUpdate.exe" Then
+                    File.Delete(_file)
+                End If
+            Next
+            For Each _folder As String In Directory.GetDirectories(Folder)
+                DeleteFilesFromFolder(_folder)
+            Next
+            Try
+                Directory.Delete(Folder)
+            Catch ex As Exception
+
+            End Try
+        End If
+    End Sub
+
+
+    Private Sub downok(sender As System.Object, e As System.ComponentModel.AsyncCompletedEventArgs)
+        LabelTxt("다운로드 완료....")
+        status += 1
+        Progress()
+    End Sub
+
+    Private Sub Progress()
+        Select Case status
+            Case 1
+                Dim versionfile As FileStream = File.Open(folder & "\temp\Lastversion", FileMode.Open)
+                Dim streamreader As New StreamReader(versionfile)
+
+
+
+                lastver = streamreader.ReadLine()
+                lastverurl = streamreader.ReadLine()
+
+                Label2.Text = "EUDEditor2 " & lastver & " Downloading..."
+
+                streamreader.Close()
+                versionfile.Close()
+
+                Download(lastverurl, "\temp\tmep.zip")
+            Case 2
+                'Dim startPath As String = "c:\example\start"
+                Dim zipPath As String = folder & "\temp\tmep.zip"
+                Dim extractPath As String = folder
+
+                Try
+                    ZipFile.ExtractToDirectory(zipPath, extractPath)
+                Catch ex As Exception
+                    MsgBox("예상치 못한 에러가 발생했습니다.")
+                    Progress()
+                    Exit Sub
+                End Try
+
+
+                DeleteFilesFromFolder(folder & "\temp")
+
+                Try
+                    Process.Start(folder & "\EUD Editor.exe")
+                Catch ex As Exception
+
+                End Try
+                Close()
+        End Select
+    End Sub
+
+
+    Private Sub LabelTxt(ByVal StrTxt As String)
+        Label1.Text = StrTxt
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DeleteFilesFromFolder(folder)
+
+        Directory.CreateDirectory(folder & "\temp")
+
+        '버전 확인
+        Download("https://github.com/Buizz/EUDEditor/raw/master/version/version", "\temp\Lastversion")
+    End Sub
 End Class
+
